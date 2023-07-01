db61474e193df2b91df319b894c30d1694ec0e70
Nicolás Sanchez Acosta <nasanchezacosta@gmail.com>
2023-07-01 10:55:40 -0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,11 @@
 import { Link } from 'react-router-dom'
 import DarkMode from './DarkMode'
-import { FaTooth } from 'react-icons/fa6'
 export default function Header() {
   return (
     <header>
       <h1>
         <span className='title'> DH</span> CLINIC{' '}
-        <span className='title'>
-        </span>
+        <span className='subtitle'>Since 2023</span>
       </h1>
       <nav>
         <Link to='/'>Home </Link>
